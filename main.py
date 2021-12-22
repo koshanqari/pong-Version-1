a18566489b51487a26ab33a2b820b3857e518c4b
Koshanqari <koshanqari@hotmail.com>
2021-12-22 17:23:52 +0530

    working scoreboard(and border), ball, bat

--- main.py
+++ main.py
@@ -3,18 +3,28 @@ import turtle
 import ball
 import time
 import bat
+#Constants
+XCOR = 1000
+YCOR = 600
 
-#Objects:
-my_score = scoreboard.Score()
-my_screen = turtle.Screen()
+#Objects initialization:
+my_score = scoreboard.Score(XCOR,YCOR)
 my_ball =  ball.Ball()
-my_bat1 = bat.Bat(350)
-my_bat2 = bat.Bat(-350)
+my_border = scoreboard.Score(XCOR, YCOR)
+my_bat1 = bat.Bat(XCOR, YCOR, XCOR/2+10)
+my_bat2 = bat.Bat(XCOR, YCOR,-(XCOR/2+10))
+
+#Object Attibutes and Methods:
+my_screen = turtle.Screen()
+my_border.make_borders()
+
 
 #Screen Properties 
-my_screen.screensize(600,600)
+my_screen.screensize(XCOR,YCOR)
 my_screen.title("K.Qari's Pong")
 my_screen.listen()
+
+
 
 #screen listening
 my_screen.onkey(my_bat1.up, "Up")
@@ -29,9 +39,9 @@ while start == True:
     time.sleep(.01)
     
     my_ball.move()
-    if my_ball.xcor() >320 or my_ball.xcor() < -320:
+    if my_ball.xcor() >XCOR/2-14 or my_ball.xcor() < -XCOR/2+14:
         my_ball.snails_law_x()
-    elif my_ball.ycor() >320 or my_ball.ycor() < -310:
+    elif my_ball.ycor() >YCOR/2-14 or my_ball.ycor() < -YCOR/2+14:
         my_ball.snails_law_y()
 
 
--- scoreboard.py
+++ scoreboard.py
@@ -6,12 +6,14 @@ class Score(Turtle):
     1. score_up(): That will increase the score once called
     2. game_over(): This will display GAME OVER on the display'''
 
-    def __init__(self):
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
         super().__init__()
         self.shape("square")
         self.hideturtle()
         self.penup()
-        self.goto(-20,290)
+        self.goto(-20,y/2)
         self.num = 0
         self.write(f'Score : {self.num}', font=('Arial', 12, 'normal'))
 
@@ -27,5 +29,14 @@ class Score(Turtle):
         self.goto(0,-20)
         self.write(f'Thanks for playing KQari\'s pong', align='center',font=('Arial', 12, 'normal'))
 
-
-    
+    def make_borders(self):
+        self.pencolor('red')
+        # self.hideturtle()
+        self.speed("fastest")
+        self.penup()
+        self.goto(-self.x/2,-self.y/2)
+        self.pendown()
+        self.goto(self.x/2,-self.y/2)
+        self.goto(self.x/2,self.y/2)
+        self.goto(-self.x/2,self.y/2)
+        self.goto(-self.x/2,-self.y/2)
