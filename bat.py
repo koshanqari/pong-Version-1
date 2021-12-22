86c1ee15b433185c9a30aa6b220bb80d3badfdae
Koshanqari <koshanqari@hotmail.com>
2021-12-22 17:20:12 +0530

    bat limitaions set acc to Xcor and Ycor

--- bat.py
+++ bat.py
@@ -1,22 +1,23 @@
+from time import sleep
 from turtle import Turtle
 class Bat(Turtle):
 
-    def __init__(self, x):
+    def __init__(self, x,y, goto):
+        self.x = x
+        self.y = y
         super().__init__()
         self.shape("square")
         self.penup()
         self.setheading(90)
         self.shapesize(stretch_len=5, stretch_wid= 1)
-        self.goto(x,0)
+        self.goto(goto,0)
 
     def up(self):
-        print(self.ycor())
-        if self.ycor() <= 250:
+        if self.ycor() <= self.y/2-100:
             self.fd(50)
 
     def down(self):
-        print(self.ycor())
-        if self.ycor() >= -250:
+        if self.ycor() >= -self.y/2+100:
             self.bk(50)
         
     
